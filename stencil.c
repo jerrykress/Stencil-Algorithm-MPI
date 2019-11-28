55959513f6fc36dcded47e7af7aff159d2b98df2
Jerry Kress <jerrykress@icloud.com>
2019-11-28 13:46:23 +0000

    fixed end bounds

--- stencil.c
+++ stencil.c
@@ -88,6 +88,7 @@ int main(int argc, char* argv[])
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &comcord);
     MPI_Cart_coords(comcord, rank, 2, coords); //Get cartesian co-ordinates of a particular rank:
     MPI_Barrier(MPI_COMM_WORLD);
+    printf("DIMS:%d %d", dims[0], dims[1]);
     printf("this is rank: %d, coord[0]: %d, coord[1]: %d \n", rank, coords[0], coords[1]);
 
     //calculate default tile size (excl. halo)
@@ -96,26 +97,26 @@ int main(int argc, char* argv[])
     //Tile receive bounds (excl. halo)
     int y_start = 1 + tile_height * (coords[1]);
     int y_end   = 0 + tile_height * (coords[1] + 1);
-    if(y_end > ny) y_end = ny; //Adjust for incomplete tile
     int x_start = 1 + tile_width  * (coords[0]);
     int x_end   = 0 + tile_width  * (coords[0] + 1);
-    if(x_end > nx) x_end = nx; //Adjust for incomplete tile
     //Halo information, encodes actual tile size
     int h_halo_size = tile_width + 2; //For complete tile
     int v_halo_size = tile_height + 2;  //For complete tile
     if (coords[0] == dims[0] - 1){
-      h_halo_size = nx - (dims[0] - 1) * tile_width + 2; //Adjust for incomplete tile
-      tile_width = h_halo_size - 2;
+      x_end = nx;
+      tile_width = x_end - x_start + 1;
+      h_halo_size = tile_width + 2;
     } 
     if (coords[1] == dims[1] - 1){
-      v_halo_size = ny - (dims[1] - 1) * tile_height + 2; //Adjust for incomplete tile
-      tile_height = v_halo_size - 2;
+      y_end = ny;
+      tile_height = y_end - y_start + 1;
+      v_halo_size = tile_height + 2;
     }
     //Setup this tile
     float* tile = _mm_malloc(sizeof(float) * (tile_width + 2) * (tile_height + 2), 64);
     float* tmp_tile = _mm_malloc(sizeof(float) * (tile_width + 2) * (tile_height + 2), 64);
     //printf("Finished tile setup on process: %d\n", rank);
-    //printf("Process %d info: %d %d %d %d %d %d %d %d\n", rank, x_start, x_end, y_start, y_end, tile_width, tile_width, h_halo_size, v_halo_size);
+    printf("Process %d info: %d %d %d %d %d %d %d %d\n", rank, x_start, x_end, y_start, y_end, tile_width, tile_width, h_halo_size, v_halo_size);
 
     //Copy from original image (excl. halo)
     for(int y_offset = 0; y_offset < tile_height; y_offset++){
@@ -148,8 +149,9 @@ int main(int argc, char* argv[])
 
       //save other tiles
       for (int receive_rank = 1; receive_rank < size; receive_rank ++){
-        int tile_meta_i[4] = {0, 0, 0, 0}; //x_start, y_start, tile_width, y_end
+        int tile_meta_i[4] = {0, 0, 0, 0}; //x_start, y_start, x_end, y_end
         MPI_Recv(&tile_meta_i[0], BUFSIZ, MPI_INT, receive_rank, tag, MPI_COMM_WORLD, &status);
+        printf("Merging to master from rank: %d, xs: %d, ys: %d, xe: %d, ye: %d\n", receive_rank, tile_meta_i[0], tile_meta_i[1],tile_meta_i[2], tile_meta_i[3]);
         for(int i = tile_meta_i[1]; i < tile_meta_i[3] + 1; i++){
           MPI_Recv(&image[i * width + tile_meta_i[0]], BUFSIZ, MPI_FLOAT, receive_rank, tag, MPI_COMM_WORLD, &status);
         }
@@ -163,7 +165,7 @@ int main(int argc, char* argv[])
 
     } else {
       //send to rank 0
-      int tile_meta_o[4] = {x_start, y_start, tile_width, y_end};
+      int tile_meta_o[4] = {x_start, y_start, x_end, y_end};
       MPI_Send(&tile_meta_o[0], 4, MPI_INT, 0, tag, MPI_COMM_WORLD);
       for(int i = 1; i < v_halo_size - 1; i++){
         MPI_Send(&tile[i*h_halo_size + 1], tile_width, MPI_FLOAT, 0, tag, MPI_COMM_WORLD);
@@ -214,7 +216,7 @@ void exchange(MPI_Comm comcord, float * tile, int h_halo_size, int v_halo_size, 
 
     // LEFT
     for (int i = 0; i < counts[0] - 1; i++) {
-      tile[i*h_halo_size + 0] = r_buffer[i + offsets[0]]; //(0,i)
+      tile[i*h_halo_size] = r_buffer[i + offsets[0]]; //(0,i)
     }
     // RIGHT
     for (int i = 0; i < counts[1] - 1; i++) {
@@ -222,7 +224,7 @@ void exchange(MPI_Comm comcord, float * tile, int h_halo_size, int v_halo_size, 
     }
     // UP
     for (int i = 0; i < counts[2] - 1; i++) {
-      tile[0*h_halo_size + i] = r_buffer[i + offsets[2]]; //(i,0)
+      tile[i] = r_buffer[i + offsets[2]]; //(i,0)
     }
     // DOWN
     for (int i = 0; i < counts[3] - 1; i++) {
