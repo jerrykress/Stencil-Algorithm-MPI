892816c14ef3b23a3a2e2a0d3447a280e7ef760e
Jerry Kress <jerrykress@icloud.com>
2019-12-12 14:14:41 +0000

    Slice OMP

--- stencil.c
+++ stencil.c
@@ -1,173 +1,162 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include "omp.h"
 #include <math.h>
+#include <omp.h>
 
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
-#define MASTER 0
 
-void stencil(const int nx, const int ny, const int width, const int height, float *image, float *tmp_image, int d0, int d1);
-void init_image(const int nx, const int ny, const int width, const int height, float *image, float *tmp_image);
-void output_image(const char *file_name, const int nx, const int ny, const int width, const int height, float *image);
+void stencil(const int nx, const int ny, const int width, const int height,
+             float *image, float *tmp_image);
+void init_image(const int nx, const int ny, const int width, const int height,
+                float *image, float *tmp_image);
+void output_image(const char *file_name, const int nx, const int ny,
+                  const int width, const int height, float *image);
 double wtime(void);
 
-int main(int argc, char *argv[]){
-  if (argc != 4){
-    fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
-
-  // Initiliase problem dimensions from command line arguments
-  int nx = atoi(argv[1]);
-  int ny = atoi(argv[2]);
-  int niters = atoi(argv[3]);
-  int width = nx + 2;
-  int height = ny + 2;
-  int dim_0 = 0;
-  int dim_1 = 0; 
-
-  // Allocate the image
-  float *image = malloc(sizeof(float) * width * height);
-  float *tmp_image = malloc(sizeof(float) * width * height);
-  init_image(nx, ny, width, height, image, tmp_image);
-
-  omp_set_num_threads(4);
-  int nthreads = 4;
-  double sq = sqrt(nthreads);
-  int prelim_dim_0 = 0;
-  int prelim_dim_1 = 0;
-
-  for (int i = 1; i <= nthreads; i++){
-    if (nthreads % i == 0){
-      if (i > prelim_dim_0 && i <= (int)floor(sq)){
-        prelim_dim_0 = i;
-      }
+int main(int argc, char *argv[])
+{
+    // Check usage
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-  }
-  prelim_dim_1 = nthreads / prelim_dim_0;
-  dim_0 = prelim_dim_0;
-  dim_1 = prelim_dim_1;
 
-  printf("NT: %d %d %d\n", nthreads, dim_0, dim_1);
+    // Initiliase problem dimensions from command line arguments
+    int nx = atoi(argv[1]);
+    int ny = atoi(argv[2]);
+    int niters = atoi(argv[3]);
 
-  double tic = wtime();
-  for (int t = 0; t < niters; ++t){
-    stencil(nx, ny, width, height, image, tmp_image, dim_0, dim_1);
-        #pragma omp barrier
-    stencil(nx, ny, width, height, tmp_image, image, dim_0, dim_1);
-        #pragma omp barrier
-  }
-  double toc = wtime();
+    // we pad the outer edge of the image to avoid out of range address issues in
+    // stencil
+    int width = nx + 2;
+    int height = ny + 2;
 
-  // Output
-  printf("------------------------------------\n");
-  printf(" runtime: %lf s\n", toc-tic);
-  printf("------------------------------------\n");
+    // Allocate the image
+    float *image = malloc(sizeof(float) * width * height);
+    float *tmp_image = malloc(sizeof(float) * width * height);
 
-  output_image(OUTPUT_FILE, nx, ny, width, height, image);
+    // Set the input image
+    init_image(nx, ny, width, height, image, tmp_image);
 
+    // Call the stencil kernel
+    double tic = wtime();
+    for (int t = 0; t < niters; ++t)
+    {
+        stencil(nx, ny, width, height, image, tmp_image);
+        stencil(nx, ny, width, height, tmp_image, image);
+    }
+    double toc = wtime();
+
+    // Output
+    printf("------------------------------------\n");
+    printf(" runtime: %lf s\n", toc - tic);
+    printf("------------------------------------\n");
+
+    output_image(OUTPUT_FILE, nx, ny, width, height, image);
+    free(image);
+    free(tmp_image);
 }
 
-void stencil(const int nx, const int ny, const int width, const int height, float *image, float *tmp_image, int dim_0, int dim_1){
-
-  int tid,coord_0,coord_1,y_start,y_end,x_start,x_end,pos,iy,ix;
-  //calculate default tile nthreads (excl. halo)
-  int tile_width = ceil((width - 2) / dim_0);   // Default tile nthreads, int col = dim_0;
-  int tile_height = ceil((height - 2) / dim_1); // Default tile nthreads, int row = nthreads;
-#pragma omp parallel shared(dim_0,dim_1,image,tmp_image,nx,ny,width,height) private(tid,coord_0,coord_1,y_start,y_end,x_start,x_end,pos,iy,ix)
-  {
-  tid = omp_get_thread_num();
-  coord_0 = (int)floor(tid / dim_0);
-  coord_1 = (int)(tid % dim_0);
-  //Tile receive bounds (excl. halo)
-  y_start = 1 + tile_height * (coord_1);
-  y_end = 0 + tile_height * (coord_1 + 1);
-  x_start = 1 + tile_width * (coord_0);
-  x_end = 0 + tile_width * (coord_0 + 1);
-  if (coord_0 == dim_0 - 1){
-    x_end = nx;
-  }
-  if (coord_1 == dim_1 - 1){
-    y_end = ny;
-  }
-    
-    #pragma omp parallel for collapse(2)
-    for (iy = y_start; iy <= y_end; iy++){
-      for (ix = x_start; ix <= x_end; ix++){
-        pos = iy * width + ix;
-        tmp_image[pos] = image[pos] * 0.6f + (image[pos - 1] + image[pos + 1] + image[pos - width] + image[pos + width]) * 0.1f;
-      }
+void stencil(const int nx, const int ny, const int width, const int height,
+             float *image, float *tmp_image)
+{
+    int i, j, location;
+#pragma omp parallel for shared(image, tmp_image) private(i, j, location)
+    for (i = 1; i < nx + 1; ++i)
+    {
+        for (j = 1; j < ny + 1; ++j)
+        {
+            //1. reduce computation by removing the repeating simple calculations
+            location = (i * height) + j;
+            tmp_image[location] = image[location] * 0.6f + (image[location - height] + image[location + height] + image[location - 1] + image[location + 1]) * 0.1f;
+        }
     }
-
-  }
 }
 
 // Create the input image
-void init_image(const int nx, const int ny, const int width, const int height, float *image, float *tmp_image){
-  // Zero everything
-  for (int i = 0; i < nx + 2; ++i){
-    for (int j = 0; j < ny + 2; ++j){
-      image[i + j * width] = 0.0;
-      tmp_image[i + j * width] = 0.0;
-    }
-  }
-
-  const int tile_size = 64;
-  // checkerboard pattern
-  for (int ib = 0; ib < nx; ib += tile_size){
-    for (int jb = 0; jb < ny; jb += tile_size){
-      if ((ib + jb) % (tile_size * 2)){
-        const int jlim = (jb + tile_size > ny) ? ny : jb + tile_size;
-        const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
-        for (int i = ib + 1; i < ilim + 1; ++i){
-          for (int j = jb + 1; j < jlim + 1; ++j){
-            image[i + j * width] = 100.0;
-          }
+void init_image(const int nx, const int ny, const int width, const int height,
+                float *image, float *tmp_image)
+{
+    // Zero everything
+    for (int j = 0; j < ny + 2; ++j)
+    {
+        for (int i = 0; i < nx + 2; ++i)
+        {
+            image[j + i * height] = 0.0;
+            tmp_image[j + i * height] = 0.0;
         }
-      }
     }
-  }
+
+    const int tile_size = 64;
+    // checkerboard pattern
+    for (int jb = 0; jb < ny; jb += tile_size)
+    {
+        for (int ib = 0; ib < nx; ib += tile_size)
+        {
+            if ((ib + jb) % (tile_size * 2))
+            {
+                const int jlim = (jb + tile_size > ny) ? ny : jb + tile_size;
+                const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
+                for (int j = jb + 1; j < jlim + 1; ++j)
+                {
+                    for (int i = ib + 1; i < ilim + 1; ++i)
+                    {
+                        image[j + i * height] = 100.0;
+                    }
+                }
+            }
+        }
+    }
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
-void output_image(const char *file_name, const int nx, const int ny, const int width, const int height, float *image){
-  // Open output file
-  FILE *fp = fopen(file_name, "w");
-  if (!fp){
-    fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
-    exit(EXIT_FAILURE);
-  }
-
-  // Ouptut image header
-  fprintf(fp, "P5 %d %d 255\n", nx, ny);
-
-  // Calculate maximum value of image
-  // This is used to rescale the values
-  // to a range of 0-255 for output
-  float maximum = 0.0;
-  for (int i = 1; i < nx + 1; ++i){
-    for (int j = 1; j < ny + 1; ++j){
-      if (image[i + j * width] > maximum)
-        maximum = image[i + j * width];
+void output_image(const char *file_name, const int nx, const int ny,
+                  const int width, const int height, float *image)
+{
+    // Open output file
+    FILE *fp = fopen(file_name, "w");
+    if (!fp)
+    {
+        fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
+        exit(EXIT_FAILURE);
     }
-  }
 
-  // Output image, converting to numbers 0-255
-  for (int i = 1; i < nx + 1; ++i){
-    for (int j = 1; j < ny + 1; ++j){
-      fputc((char)(255.0 * image[i + j * width] / maximum), fp);
+    // Ouptut image header
+    fprintf(fp, "P5 %d %d 255\n", nx, ny);
+
+    // Calculate maximum value of image
+    // This is used to rescale the values
+    // to a range of 0-255 for output
+    double maximum = 0.0;
+    for (int j = 1; j < ny + 1; ++j)
+    {
+        for (int i = 1; i < nx + 1; ++i)
+        {
+            if (image[j + i * height] > maximum)
+                maximum = image[j + i * height];
+        }
     }
-  }
 
-  // Close the file
-  fclose(fp);
+    // Output image, converting to numbers 0-255
+    for (int j = 1; j < ny + 1; ++j)
+    {
+        for (int i = 1; i < nx + 1; ++i)
+        {
+            fputc((char)(255.0 * image[j + i * height] / maximum), fp);
+        }
+    }
+
+    // Close the file
+    fclose(fp);
 }
 
 // Get the current time in seconds since the Epoch
-double wtime(void){
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return tv.tv_sec + tv.tv_usec * 1e-6;
+double wtime(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
 }
